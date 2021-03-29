f17d4fa8ef0ec6af8f3c7f48e7a6883bdc1ed7f6
Clark <ck0913660913@gmail.com>
2021-03-29 11:38:42 +0800

    No chance

--- 35.practice.py
+++ 35.practice.py
@@ -4,12 +4,15 @@ password = 'a123456'
 i = 3 # 剩下機會
 #while True: # 高階 i>0
 while i > 0:
+	i = i-1
 	pwd = input('PWD = ? ')
 	if pwd == password:
 		print('OK!')
 		break
 	else:
-		i = i-1
-		print('Wrong, U have only' , i, 'chance')
+		if i > 0:
+			print('Wrong, U have only' , i, 'chance')
 		#if i == 0: # 高階 不用寫
 		#	break # 高階 不用寫
+		else:
+			print('No chance')
